1be35ce54e8a2af0cde4a91527455e35e8265ecb
Abhishekyadav26 <aky732244@gmail.com>
2023-09-03 13:47:02 +0530

    adding new file

---
+++ prime_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("hey user enter the integer :");
+    scanf("%d",&n);
+    int a = 0;
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            a = 1;
+            break;
+        }    
+    }
+    if(a==1){
+        printf("it is a composite number ");   
+    }
+    else if(a==0){
+        ("it is a prime number");
+    }
+    else{
+        printf("this number is composite ");
+    }
+    return 0;
+}
